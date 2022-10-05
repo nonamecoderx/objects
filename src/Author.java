0300d9aca509592b20391841eef01766d48d8840
Иван <ivankiseliov@rambler.ru>
2022-10-05 21:03:10 +0300

    dz 2

--- src/Author.java
+++ src/Author.java
@@ -1,18 +1,43 @@
+import java.util.Objects;
+
 public class Author {
-    public String nameAuthor;
-    public String surnameAuthor;
-    public Author ( String nameAuthor, String surnameAuthor){
+    private String nameAuthor;
+    private String surnameAuthor;
+
+    public Author(String nameAuthor, String surnameAuthor) {
         this.nameAuthor = nameAuthor;
         this.surnameAuthor = surnameAuthor;
     }
-    public String getNameAuthor(){
+
+    public String getNameAuthor() {
         return this.nameAuthor;
     }
-    public String getSurNameAuthor(){
+
+    public String getSurNameAuthor() {
         return this.surnameAuthor;
     }
+
     public String toString() {
         return "Имя автора " + this.nameAuthor + "Фамилия автора " + this.surnameAuthor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Author author = (Author) o;
+        return Objects.equals(nameAuthor, author.nameAuthor) &&
+                Objects.equals(surnameAuthor, author.surnameAuthor);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameAuthor, surnameAuthor);
     }
 }
 
--- src/Book.java
+++ src/Book.java
@@ -1,20 +1,47 @@
+import java.util.Objects;
+
 public class Book {
     private String bookName;
     Author author;
     private int bookDate;
+
     public Book(String bookName, Author author, int bookDate) {
         this.bookName = bookName;
         this.bookDate = bookDate;
         this.author = author;
     }
+
     public String getName() {
         return this.bookName;
     }
+
     public int getDate() {
         return this.bookDate;
     }
 
     public void setBookDate(int bookDate) {
         this.bookDate = bookDate;
+    }
+
+    public String toString() {
+        return this.bookName + "by" + this.author + "published in " + this.bookDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+        return false;
+        }
+        Book book = (Book) o;
+        return bookDate == book.bookDate && Objects.equals(bookName, book.bookName)&&
+        Objects.equals(author, book.author);
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(bookName, author, bookDate);
     }
 }
